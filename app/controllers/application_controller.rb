bbdd494319066ec02ddc7149f069a697e64ee39f
Tom Levy <tom@solveit.fr>
2020-03-02 17:37:35 +0100

    setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def default_url_options
+  { host: ENV["www.foundaters.com"] || "localhost:3000" }
+end
 end
